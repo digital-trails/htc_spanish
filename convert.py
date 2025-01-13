e6a8cda208eeff68f229dda697a37779bf29b63a
mrucker <rucker.mark@gmail.com>
2025-01-13 09:43:44 -0500

    Added several scripts in new format.

--- convert.py
+++ convert.py
@@ -1,20 +1,36 @@
 import json
 from pathlib import Path
 
-with open('./src/scripts/intro.json', encoding='utf-8') as f:
-    obj = json.load(f)
+files = ['eod', 'instructions', 'disclaimer', 'what_is_anxiety']
 
-page_index = 1
-for page_group in obj['page_groups']:
-    title = page_group.get('title',None)
-    for page in page_group['pages']:
-        page_obj = {
-            'title': title,
-            'blocks': {
-                'type': 'VerticalLayout',
-                'elements':page['elements']
+files = files[1:]
+
+for file in files:
+
+    with open(f'./src/scripts/{file}.json', encoding='utf-8') as f:
+        obj = json.load(f)
+
+    page_index = 1
+
+    if 'pages' in obj:
+        obj = {'page_groups': [obj]}
+
+    for page_group in obj['page_groups']:
+        props = page_group.copy()
+        props.pop('pages')
+        for page in page_group['pages']:
+            props.update(page)
+            props.pop('elements')
+            page_obj = {
+                **props,
+                'blocks': {
+                    'type': 'VerticalLayout',
+                    'elements':page['elements']
+                }
             }
-        }
-        with open(f"./src/scripts/pages/intro/{page_index}.json","w+",encoding='utf-8') as f:
-            json.dump(page_obj,f, indent=4, ensure_ascii=False)
-        page_index+=1
+
+            directory = Path(f"./src/scripts/pages/{file}")
+            directory.mkdir(parents=True, exist_ok=True)
+            with open(directory / f"{page_index}.json", "w+", encoding='utf-8') as f:
+                json.dump(page_obj, f, indent=4, ensure_ascii=False)
+            page_index+=1
