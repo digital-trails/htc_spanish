933b526bbbf9742a233571ee87c0746ed9cc96a2
Mark Rucker <rucker.mark@gmail.com>
2025-01-29 22:09:12 -0500

    Moved image validation to its own folder.

--- validate/validate_image_paths.py
+++ validate/validate_image_paths.py
@@ -1,31 +1,31 @@
 from pathlib import Path
 import json
 
-def process_file(obj):
+def validate_file(obj):
     if isinstance(obj,dict):
         for k,v in obj.items():
             if k.lower() in ['url','file'] and not Path(f'./src/{v}').exists():
                 yield v
             else:
-                yield from process_file(v)
+                yield from validate_file(v)
     if isinstance(obj,list):
         for o in obj:
-            yield from process_file(o)
+            yield from validate_file(o)
 
-def process_dir(dir):
+def validate_dir(dir):
     for p in Path(dir).iterdir():
 
         if(p.is_dir()):
-            process_dir(p)
+            validate_dir(p)
             continue
 
         with open(p, encoding='utf-8') as f:
             
-            bad_links = list(process_file(json.load(f)))
+            bad_links = list(validate_file(json.load(f)))
 
             if bad_links:
                 print(p)
                 for l in bad_links:
                     print(f'  {l}')
 
-process_dir('./src/flows')
+validate_dir('./src/flows')
